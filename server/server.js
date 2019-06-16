d6a2e7f92a0251350091cff86356080ed980da38
unknown <cubamc@gmail.com>
2019-06-15 23:44:40 -0400

    server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "../build")))
+
+
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function (request, res) {
+    res.sendFile(
+        path.resolve(__dirname, "../build/index.html")
+    )
+
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
