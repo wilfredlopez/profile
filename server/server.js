987d6180d39b556c71e44d7774bcc9e1cff0da16
unknown <cubamc@gmail.com>
2019-06-15 23:50:51 -0400

    setting static

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const path = require('path')
 const app = express()
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../build")))
+app.use(express.static(path.resolve(__dirname, "..", "build")))
 
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("*", function (request, res) {
     res.sendFile(
-        path.resolve(__dirname, "../build/index.html")
+        path.resolve(__dirname, "..", "build", "index.html")
     )
 
 })
