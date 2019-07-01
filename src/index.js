6ab6ee578aa46dd1b7439970f65062c2854239ed
unknown <cubamc@gmail.com>
2019-06-30 23:18:55 -0400

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { StoreProvider, createStore } from "easy-peasy"
 import model from "./model";  //Easy-Peasy React Redux Model
 import 'bootstrap/dist/css/bootstrap.min.css' //YOU CAN REMOVE IF ITS NOT REQUIRED
@@ -11,12 +12,14 @@ import 'semantic-ui-less/semantic.less'
 
 const store = createStore(model)
 
+//replaced the browser router for hash router for it to work bettter on github pages
+//<BrowserRouter basename="/">
 ReactDOM.render(
-    <BrowserRouter basename="/">
+    <HashRouter>
         <StoreProvider store={store}>
             <App />
         </StoreProvider>
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('root'));
 
 serviceWorker.unregister();
