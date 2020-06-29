22219964491e77d9402c28ce21f688eaac70ac19
Wilfred Lopez <cubamc@gmail.com>
2020-06-29 14:56:03 -0400

    mayor v change

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withImages = require("next-images");
 module.exports = withImages({
   esModule: true,
   // useFileSystemPublicRoutes: false,
+  distDir: "dist",
   webpack(config, options) {
     // Fixes npm packages that depend on `fs` module
     config.node = {
