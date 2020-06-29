dcc41ef2e553f0b36970e31ddd2b4691cf512832
Wilfred Lopez <cubamc@gmail.com>
2020-06-29 16:19:14 -0400

    adding out dir

---
+++ out/_next/static/bx3FnamqHDlggSHXxYB1E/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/projects":[a,b,c]}}("static\u002Fchunks\u002F3230ce31.55a007d0bb7db8f0a966.js","static\u002Fchunks\u002F95b64a6e.fad6072a5d28a2b5789d.js","static\u002Fchunks\u002F6664ea0f7c66977c944e01255fa016ed3482115f.0505609e41963c549f15.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
