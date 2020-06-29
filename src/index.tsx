5bb9e35518ad89d158a79c79e8f6c4bc83955133
Wilfred Lopez <cubamc@gmail.com>
2020-06-29 13:34:12 -0400

    commented old non-nextjs code

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-// import { BrowserRouter } from 'react-router-dom'
-import { HashRouter } from "react-router-dom";
-import RootProvider from "RootProvider";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+// // import { BrowserRouter } from 'react-router-dom'
+// import { HashRouter } from "react-router-dom";
+// import RootProvider from "RootProvider";
 
-//replaced the browser router for hash router for it to work bettter on github pages
-//<BrowserRouter basename="/">
-ReactDOM.render(
-  <RootProvider>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </RootProvider>,
-  document.getElementById("root"),
-);
+// //replaced the browser router for hash router for it to work bettter on github pages
+// //<BrowserRouter basename="/">
+// ReactDOM.render(
+//   <RootProvider>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </RootProvider>,
+//   document.getElementById("root"),
+// );
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
+export {};
