d95875cdcfdcdf6ffa49197631583395932dd62e
wilfredlopez <cubamc@gmail.com>
2021-04-15 18:25:51 -0400

    added api url as default

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const CONTACT_EMAIL = 'wilfredlopez@outlook.com'
-export const EMAIL_API_URL = process.env.NEXT_PUBLIC_EMAIL_API_URL || 'http://localhost:5300/api/email'
+export const EMAIL_API_URL = process.env.NEXT_PUBLIC_EMAIL_API_URL || 'https://wilfred-email-backend.herokuapp.com/api/email'
