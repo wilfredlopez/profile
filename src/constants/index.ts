9688b7db50e217b8e94d668032b045b9947e9ebb
wilfredlopez <cubamc@gmail.com>
2021-04-29 13:20:55 -0400

    added get request to email heroku api to make sure is up when client visits contact page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export const CONTACT_EMAIL = 'wilfredlopez@outlook.com'
 export const EMAIL_API_URL = process.env.NEXT_PUBLIC_EMAIL_API_URL || 'https://wilfred-email-backend.herokuapp.com/api/email'
+export const EMAIL_API_ROOT_URL = 'https://wilfred-email-backend.herokuapp.com/'
